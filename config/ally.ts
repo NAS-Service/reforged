5953b5d6ac01386d0fd5030d0e1234a383fe4a62
Thibault <contactazarox@gmail.com>
2023-05-31 01:50:47 +0200

    feat: implement ally

---
+++ config/ally.ts
@@ -0,0 +1,78 @@
+/**
+ * Config source: https://git.io/JOdi5
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { AllyConfig } from '@ioc:Adonis/Addons/Ally'
+
+/*
+|--------------------------------------------------------------------------
+| Ally Config
+|--------------------------------------------------------------------------
+|
+| The `AllyConfig` relies on the `SocialProviders` interface which is
+| defined inside `contracts/ally.ts` file.
+|
+*/
+const allyConfig: AllyConfig = {
+  /*
+	|--------------------------------------------------------------------------
+	| Google driver
+	|--------------------------------------------------------------------------
+	*/
+  google: {
+    driver: 'google',
+    clientId: Env.get('GOOGLE_CLIENT_ID'),
+    clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
+    callbackUrl: Env.get('GOOGLE_CALLBACK_URL'),
+  },
+  /*
+	|--------------------------------------------------------------------------
+	| Twitter driver
+	|--------------------------------------------------------------------------
+	*/
+  twitter: {
+    driver: 'twitter',
+    clientId: Env.get('TWITTER_CLIENT_ID'),
+    clientSecret: Env.get('TWITTER_CLIENT_SECRET'),
+    callbackUrl: Env.get('TWITTER_CALLBACK_URL'),
+  },
+  /*
+	|--------------------------------------------------------------------------
+	| Discord driver
+	|--------------------------------------------------------------------------
+	*/
+  discord: {
+    driver: 'discord',
+    clientId: Env.get('DISCORD_CLIENT_ID'),
+    clientSecret: Env.get('DISCORD_CLIENT_SECRET'),
+    callbackUrl: Env.get('DISCORD_CALLBACK_URL'),
+  },
+  /*
+  |--------------------------------------------------------------------------
+  | LinkedIn driver
+  |--------------------------------------------------------------------------
+  */
+  linkedin: {
+    driver: 'linkedin',
+    clientId: Env.get('LINKEDIN_CLIENT_ID'),
+    clientSecret: Env.get('LINKEDIN_CLIENT_SECRET'),
+    callbackUrl: Env.get('LINKEDIN_CALLBACK_URL'),
+  },
+  /*
+  |--------------------------------------------------------------------------
+  | Facebook driver
+  |--------------------------------------------------------------------------
+  */
+  facebook: {
+    driver: 'facebook',
+    clientId: Env.get('FACEBOOK_CLIENT_ID'),
+    clientSecret: Env.get('FACEBOOK_CLIENT_SECRET'),
+    callbackUrl: Env.get('FACEBOOK_CALLBACK_URL'),
+  },
+}
+
+export default allyConfig
---
+++ contracts/ally.ts
@@ -0,0 +1,31 @@
+/**
+ * Contract source: https://git.io/JOdiQ
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+declare module '@ioc:Adonis/Addons/Ally' {
+	interface SocialProviders {
+    google: {
+      config: GoogleDriverConfig
+      implementation: GoogleDriverContract
+    }
+		twitter: {
+			config: TwitterDriverConfig
+			implementation: TwitterDriverContract
+		}
+    discord: {
+      config: DiscordDriverConfig
+      implementation: DiscordDriverContract
+    }
+    linkedin: {
+      config: LinkedInDriverConfig
+      implementation: LinkedInDriverContract
+    }
+    facebook: {
+      config: FacebookDriverConfig
+      implementation: FacebookDriverContract
+    }
+	}
+}
