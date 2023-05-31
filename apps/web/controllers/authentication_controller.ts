95016f69a005e27514693c89e607c14ab7100502
Thibault <contactazarox@gmail.com>
2023-05-31 03:11:25 +0200

    feat(authentication): add base controller

--- apps/web/controllers/authentication_controller.ts
+++ apps/web/controllers/authentication_controller.ts
@@ -1,7 +1,25 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import SocialAuth from 'apps/web/services/social_auth'
+import User from 'Domains/users/models/User'
 
 export default class AuthenticationController {
   public async showLogin({ view }: HttpContextContract) {
     return view.render('manager::views/welcome')
+  }
+
+  public async redirect({ ally, params }: HttpContextContract) {
+    return await ally.use(params.provider).redirect()
+  }
+
+  public async callback({ ally, auth, params, response }: HttpContextContract) {
+    const socialUser = await ally.use(params.provider).user()
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await new SocialAuth(socialUser, params.provider).onFindOrCreate(async (user: User) => {
+      await auth.login(user)
+
+      response.redirect().toRoute('aa')
+    })
   }
 }
