fa2f5c89d2f7a20cc0958f0ee130fd0c132b3e55
Thibault <contactazarox@gmail.com>
2023-05-31 03:12:37 +0200

    feat(authentication): add routes

--- apps/web/routes.ts
+++ apps/web/routes.ts
@@ -2,4 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
   Route.get('/authentication/login', 'authentication_controller.showLogin').as('home')
+  Route.get('/oauth/:provider/redirect', 'authentication_controller.redirect')
+  Route.get('/oauth/:provider/callback', 'authentication_controller.callback')
 }
