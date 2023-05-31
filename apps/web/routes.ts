fb0a2e61c638c3c3c5588db592019f5025c73a12
Thibault <contactazarox@gmail.com>
2023-05-31 03:42:50 +0200

    feat(authentication): add pages sign up

--- apps/web/routes.ts
+++ apps/web/routes.ts
@@ -1,7 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
-  Route.get('/authentication/login', 'authentication_controller.showLogin').as('home')
-  Route.get('/oauth/:provider/redirect', 'authentication_controller.redirect')
+  Route.get('/authentication/login', 'authentication_controller.showSignIn').as('login')
+  Route.get('/authentication/signup', 'authentication_controller.showSignUp').as('signup')
+  Route.get('/oauth/:provider/redirect', 'authentication_controller.redirect').as('oauth.redirect')
   Route.get('/oauth/:provider/callback', 'authentication_controller.callback')
 }
