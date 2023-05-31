ddabf4405927c74f234384aba9ef8b85a24feda1
Thibault <contactazarox@gmail.com>
2023-05-31 16:01:44 +0200

    feat(authenticate): add crete user

--- apps/web/routes.ts
+++ apps/web/routes.ts
@@ -3,8 +3,9 @@ import Route from '@ioc:Adonis/Core/Route'
 export default () => {
   Route.group(() => {
     Route.get('/login', 'auth/authentication_controller.showSignIn').as('showLogin')
-    Route.get('/signup', 'auth/authentication_controller.showSignUp').as('showSignup')
-    Route.post('/store', 'auth/authentication_controller.login').as('login')
+    Route.post('/login', 'auth/authentication_controller.login').as('login')
+    Route.get('/signup', 'user_controller.create').as('showSignup')
+    Route.post('/signup', 'user_controller.store').as('signup')
   }).prefix('/authentication')
 
   Route.group(() => {
