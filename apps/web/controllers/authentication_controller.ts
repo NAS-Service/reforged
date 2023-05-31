d265dda14881da3aef1b2a128bce6917ba8121d0
Thibault <contactazarox@gmail.com>
2023-05-31 04:01:03 +0200

    feat(authentication): add two factor verify

--- apps/web/controllers/authentication_controller.ts
+++ apps/web/controllers/authentication_controller.ts
@@ -1,4 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'Domains/users/models/User'
+import twoFactor from 'node-2fa'
 
 export default class AuthenticationController {
   public async showSignIn({ view }: HttpContextContract) {
@@ -9,15 +11,38 @@ export default class AuthenticationController {
     return view.render('web::views/authentication/store')
   }
 
-  public async login({ request, response, auth, view }: HttpContextContract) {
+  public async login({ request, response, session, auth, view }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
     const user = await auth.use('web').verifyCredentials(email, password)
 
     if (user.isTwoFactorEnabled) {
+      session.put('login.id', user.id)
       return view.render('')
     }
 
+    session.forget('login.id')
+    session.regenerate()
     await auth.login(user)
     response.redirect().toRoute('aa')
+  }
+
+  public async twoFactorChallenge({ request, session, auth, response }) {
+    const { code, recoveryCode } = request.only(['code', 'recoveryCode'])
+    const user = await User.query().where('id', session.get('login.id')).first()
+    if (code) {
+      const isValid = await twoFactor.verifyToken(user!.twoFactorSecret!, code)
+      if (isValid) {
+        await auth.login(user)
+        return response.redirect('/')
+      }
+    } else if (recoveryCode) {
+      const codes = user?.twoFactorRecoveryCodes ?? []
+      if (codes.includes(recoveryCode)) {
+        user!.twoFactorRecoveryCodes = codes.filter((c) => c !== recoveryCode)
+        await user!.save()
+        await auth.login(user)
+        return response.redirect('/')
+      }
+    }
   }
 }
