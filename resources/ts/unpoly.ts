e7c6445096ff22e2b87b4907af25890e2a98dcd8
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-06-04 20:08:28 +0200

    feat: configure unpoly

--- resources/ts/unpoly.ts
+++ resources/ts/unpoly.ts
@@ -9,8 +9,19 @@ unpoly.fragment.config.mainTargets.push('[layout-main]')
 
 unpoly.link.config.preloadSelectors.push('a[href]')
 unpoly.link.config.followSelectors.push('a[href]')
+unpoly.link.config.noFollowSelectors.push('a[data-unfollow]')
 
 unpoly.form.config.submitSelectors.push(['form'])
+
+unpoly.on('up:fragment:loaded', (event) => {
+  if (event.response.getHeader('X-Full-Reload')) {
+    // Prevent the fragment update and don't update browser history
+    event.preventDefault()
+
+    // Make a full page load for the same request.
+    event.request.loadPage()
+  }
+})
 
 unpoly.on('up:fragment:loaded', (event) => {
   if (event.response.getHeader('X-Full-Reload')) {
