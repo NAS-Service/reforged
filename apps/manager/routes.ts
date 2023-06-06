fb59986cb93e63aca18c057deb8c21a92379844a
Thibault <contactazarox@gmail.com>
2023-06-06 20:54:07 +0200

    feat(permission): add view and routes

--- apps/manager/routes.ts
+++ apps/manager/routes.ts
@@ -1,7 +1,18 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
-  Route.get('test', ({ response }) => {
-    return response.send('test')
+  Route.group(() => {
+    Route.get('/', 'role_controller.index').as('index')
+    Route.get('/:id', 'role_controller.show').as('show')
+    Route.post('/create', 'role_controller.store').as('store')
+    Route.delete('/:id', 'role_controller.destroy').as('destroy')
   })
+    .prefix('roles')
+    .as('roles')
+
+  Route.group(() => {
+    Route.get('/', 'permissions_controller.index').as('index')
+  })
+    .prefix('permissions')
+    .as('permissions')
 }
