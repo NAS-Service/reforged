b50e1c43c635e63b4f0911383e410b03b0ce0c50
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-07-06 21:02:53 +0200

    feat: define manager sections

--- apps/manager/controllers/app_controller.ts
+++ apps/manager/controllers/app_controller.ts
@@ -2,6 +2,6 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AppController {
   public async index({ view }: HttpContextContract) {
-    return view.render('welcome')
+    return view.render('manager::views/index')
   }
 }
--- apps/manager/routes.ts
+++ apps/manager/routes.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
+  Route.get('/', 'app_controller.index').as('home')
   Route.group(() => {
     Route.get('/', 'role_controller.index').as('index')
     Route.get('/:id', 'role_controller.show').as('show')
