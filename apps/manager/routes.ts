f5c50ea636727bdea0d0e52e9093743958c9f621
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-07-07 00:24:43 +0200

    feat: implement manager navbar

--- apps/manager/routes.ts
+++ apps/manager/routes.ts
@@ -3,17 +3,19 @@ import Route from '@ioc:Adonis/Core/Route'
 export default () => {
   Route.get('/', 'app_controller.index').as('home')
   Route.group(() => {
-    Route.get('/', 'role_controller.index').as('index')
-    Route.get('/:id', 'role_controller.show').as('show')
-    Route.post('/create', 'role_controller.store').as('store')
-    Route.delete('/:id', 'role_controller.destroy').as('destroy')
-  })
-    .prefix('roles')
-    .as('roles')
+    Route.group(() => {
+      Route.get('/', 'role_controller.index').as('index')
+      Route.get('/:id', 'role_controller.show').as('show')
+      Route.post('/create', 'role_controller.store').as('store')
+      Route.delete('/:id', 'role_controller.destroy').as('destroy')
+    })
+      .prefix('roles')
+      .as('roles')
 
-  Route.group(() => {
-    Route.get('/', 'permissions_controller.index').as('index')
-  })
-    .prefix('permissions')
-    .as('permissions')
+    Route.group(() => {
+      Route.get('/', 'permissions_controller.index').as('index')
+    })
+      .prefix('permissions')
+      .as('permissions')
+  }).prefix('users')
 }
