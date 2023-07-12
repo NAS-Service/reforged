0e98f4e2e60712b0155a87db124db33544135ddb
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-07-12 22:51:30 +0200

    fix: error when filter don't have any query url params

--- start/view.ts
+++ start/view.ts
@@ -1,6 +1,10 @@
 import View from '@ioc:Adonis/Core/View'
 
 View.global('httpParams', (request) => {
+  if (!request?.request?._parsedUrl.query) {
+    return {}
+  }
+
   return request.request._parsedUrl.query.split('&').reduce((acc, value) => {
     const [key, val] = value.split('=')
     return { ...acc, [key]: val }
